5da85a8e5673e8fdc27a9cff907cce380a383299
meetsarvaiya2605 <meetsarvaiya41@gmail.com>
2026-08-17 23:37:23 +0530

    seedcommit

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./food_delivery.db"
+SQLALCHEMY_DATABASE_URL = "https://hxxxbmdszvypnjgomamz.supabase.co"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
